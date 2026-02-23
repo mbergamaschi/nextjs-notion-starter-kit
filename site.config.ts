28d32c5246a9b4dcc733ab6e03e43611dcd4bc14
mbergamaschi <luamicaelb@gmail.com>
2026-02-23 20:50:02 -0300

    Update social media links in site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -22,7 +22,10 @@ export default siteConfig({
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-   youtube: 'channel/MicaelBergamaschi', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  instagram: 'micaelbergamaschi',
+  youtube: '@MicaelBergamaschi', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  medium: '@mbergamaschi',
+  whatsapp: '5521979528800',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
