6ffb103f67595af734c706f1b447779156497197
mbergamaschi <luamicaelb@gmail.com>
2026-02-23 18:37:25 -0300

    Update site configuration with new author and domain

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Micael Bergamaschi',
+  domain: 'mbergamaschi.com',
+  author: 'Micael Bergamaschi',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Micael Bergamaschi: Fotógrafo de circo, professor de aéreos, diretor criativo',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: '',
+  // github: '',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
