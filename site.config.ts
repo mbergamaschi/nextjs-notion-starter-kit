b1aeb90550fa14e7ae0c96abd356905f34040b4b
mbergamaschi <luamicaelb@gmail.com>
2026-02-23 21:53:00 -0300

    Disable social media links in site.config.ts
    Comment out social media links in site configuration.

--- site.config.ts
+++ site.config.ts
@@ -22,10 +22,10 @@ export default siteConfig({
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  instagram: 'micaelbergamaschi',
-  youtube: '@MicaelBergamaschi', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-  medium: '@mbergamaschi',
-  whatsapp: '5521979528800',
+  //instagram: 'micaelbergamaschi',
+  //youtube: '@MicaelBergamaschi', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  //medium: '@mbergamaschi',
+  //whatsapp: '5521979528800',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
