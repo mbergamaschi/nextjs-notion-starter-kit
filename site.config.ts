69f717ab87df02af1605e132b4a1646fd0754eb8
mbergamaschi <luamicaelb@gmail.com>
2026-02-25 12:03:48 -0300

    Enable Redis caching in site configuration
    Enabled Redis caching for generated preview images.

--- site.config.ts
+++ site.config.ts
@@ -39,7 +39,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: true,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
@@ -48,7 +48,6 @@ export default siteConfig({
    pageUrlOverrides: {
      '/aulas-manaca': '6b57f85f046b47d8b0d5be48f98fba9b',
      '/mostra-enclo-2026': '2ebfcab3c1fa8076ba83d9da3b28ac50',
-     '/copyright-notice': '68fd308bf71441079a75f2a7b20ce798',
      '/termos': '312fcab3c1fa80e287adf6203e4789a4',
      '/cfc': 'da26539d457c4c29aebf5e807d4f28f5',
      '/ccc': '8a98a09e888a44da87ba5a382a0a2f10',
@@ -57,7 +56,6 @@ export default siteConfig({
      '/2112': '04ade3c6e4c2476da44bca678d2b23db',
      '/artistas': '2ebfcab3c1fa80568603c312361abe7c',
      '/modalidades': '2ebfcab3c1fa8061aee6f0695afa0177',
-     '/notas-direitos-autorais': '6052c6e813d54e58b4013d9d5d7c705b',
      '/mestras': '2ecfcab3c1fa8057b382d2777f347d95',
      '/espetaculos': '311fcab3c1fa80ed9390eae3ee303e12'
    },
